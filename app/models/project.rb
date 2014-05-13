5bf31acf89d7b78c2f782ef93b91ae0332dbf3f6
Kangie Chan <aries0420@gmail.com>
2014-05-12 23:36:34 -0700

    Added invalid data spec for creating_a_project_test.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  validates :description, presence: true
+  validates :title, length: { in: 4..255 }
 end
--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -11,4 +11,19 @@ feature "As the site owner, I want to add a project item so that I can show off 
     page.text.must_include "Caligraphy"
     page.text.must_include "script"
   end
+
+  scenario "new project has invalid data" do
+    # Given invalid project data is entered in a form
+    visit new_project_path
+    fill_in "Title", with: "Q"
+
+    # When the form is submitted with a short name and missing technologies_used field
+    click_on "Create Project"
+
+    # Then the form should be displayed again, with an error message
+    current_path.must_match /projects$/
+    page.text.must_include "Project could not be saved"
+    page.text.must_include "Title is too short"
+    page.text.must_include "Description can't be blank"
+  end
 end
